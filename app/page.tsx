9d3e9155d716bed229513f5c456b21bde39dc72a
M0-o <berrami.mouad2@gmail.com>
2026-07-06 06:21:53 +0100

    bug fix : page not updating data after build

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma"
 import PageClient from "@/components/page-client"
 
+export const dynamic = "force-dynamic"
+
 export default async function Home() {
   const services = await prisma.service.findMany({
     orderBy: { createdAt: "asc" },
